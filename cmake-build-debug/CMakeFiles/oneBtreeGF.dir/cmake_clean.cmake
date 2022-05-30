f49d6283430f242b3883fe3aed6e59da40870248
NCC <ncc@192.168.1.4>
2022-05-30 10:05:02 +0630

    firstcommit

---
+++ cmake-build-debug/CMakeFiles/oneBtreeGF.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/oneBtreeGF.dir/main.cpp.o"
+  "oneBtreeGF"
+  "oneBtreeGF.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/oneBtreeGF.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
